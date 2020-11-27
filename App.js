16aa3fbab5168891e78648fe4f4f3317b2d63f33
AbdullahJanKhan <abdullah.jankhan445@hotmail.com>
2020-11-27 10:30:38 +0500

    Added Input Texts for showing discounted value and save amount

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useDebugValue, useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App(){
+    const [getPrice, setPrice] = useState('');
+    const [getDiscount, setDiscount] = useState('');
+
+    const [getDiscountValue, setDiscountValue] = useState('');
+    const [getSaveAmount, setSaveAmount] = useState('');
+
+    const calculate = ()=>{
+        const actual = Number(getPrice)
+        const discount = Number(getDiscount)
+        const discountValue = actual - (actual*(discount/100));
+        const save = actual - discountValue
+        setDiscountValue(String(discountValue))
+        setSaveAmount(String(save))
+    }
 
     return(
         <View style={styles.container}>
             <View style={styles.inputTextContainer}>
                 <View style={styles.inputTextStyles}>
-                    <TextInput keyboardType='decimal-pad' placeholder = 'Enter Actual Price' />
+                    <Text>Orignal Price: </Text>
+                    <TextInput keyboardType='decimal-pad' value={getPrice} onChangeText={(data)=>setPrice(data)} placeholder = 'Enter Actual Price' />
                 </View>
                 <View style={styles.inputTextStyles}>
-                    <TextInput keyboardType='decimal-pad' placeholder = 'Enter Discount' />
+                    <Text>Discount Avaliable: </Text>
+                    <TextInput keyboardType='decimal-pad' value={getDiscount} onChangeText={(data)=>setDiscount(data)} placeholder = 'Enter Discount' />
                 </View>
+            </View>
+            <View style={styles.inputTextContainer}>
+                <View style={styles.inputTextStyles}>
+                    <Text>Discounted Value: </Text>
+                    <TextInput style={styles.textStyle} editable={false} value={getDiscountValue} placeholder = 'Discounted Price' />
+                </View>
+                <View style={styles.inputTextStyles}>
+                    <Text>You Save: </Text>
+                    <TextInput style={styles.textStyle} editable={false} value={getSaveAmount} placeholder = 'You Save' />
+                </View>
+            </View>
+            <View style={styles.buttonStyle}>
+                <Button title='Calculate' color='grey' onPress={()=>calculate()}/>
             </View>
         </View>
     );
@@ -25,7 +54,7 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     textStyle:{
-      fontSize: 21
+      color:'black'
     },
     inputTextContainer:{
       flexDirection: 'row',
